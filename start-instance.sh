672f73f6aa1ffac3e8214b106e77ca8caaa25f72
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-02-28 14:28:36 +1100

    Fix equality test.

--- start-instance.sh
+++ start-instance.sh
@@ -12,7 +12,7 @@ JUPYTERHUB_IMAGE_NAME=${JUPYTERHUB_IMAGE_NAME:-$IMAGE}
 
 JUPYTERHUB_API_URL=http://$JUPYTERHUB_HOST_NAME:8080/hub/api
 
-if [ x"$JUPYTERHUB_STORAGE_TYPE" == "persistent" ]; then
+if [ x"$JUPYTERHUB_STORAGE_TYPE" == x"persistent" ]; then
     PVC=pvc-$JUPYTER_NOTEBOOK_USER
 
     if oc get pvc/$PVC; then
