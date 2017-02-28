363f5c541f6c015dcf8572471ae908caa99d1a47
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-02-28 19:21:46 +1100

    Set root as admin for JupyterHub.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -4,3 +4,4 @@ c.JupyterHub.proxy_api_port = 8082
 c.JupyterHub.spawner_class = 'openshiftspawner.OpenShiftSpawner'
 c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
 c.Spawner.http_timeout = 60
+c.Authenticator.admin_users = set(['root'])
