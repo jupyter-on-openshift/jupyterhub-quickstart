cfbf9f5087ab157668161576b6b22fae938f9637
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-03-01 10:33:14 +1100

    Move database and session cookie files into separate directory so can use persistent volume.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -3,5 +3,7 @@ c.JupyterHub.hub_port = 8081
 c.JupyterHub.proxy_api_port = 8082
 c.JupyterHub.spawner_class = 'openshiftspawner.OpenShiftSpawner'
 c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+c.JupyterHub.cookie_secret_file = '/opt/app-root/data/cookie_secret'
+c.JupyterHub.db_url = '/opt/app-root/data/jupyterhub.sqlite')
 c.Spawner.http_timeout = 60
 c.Authenticator.admin_users = set(['root'])
