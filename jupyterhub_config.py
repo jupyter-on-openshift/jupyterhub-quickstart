76cc48685746099817cb3d4b0968e70a51130e38
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-02-28 18:21:41 +1100

    Increase spawner timeout on responding to HTTP request.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -3,3 +3,4 @@ c.JupyterHub.hub_port = 8081
 c.JupyterHub.proxy_api_port = 8082
 c.JupyterHub.spawner_class = 'openshiftspawner.OpenShiftSpawner'
 c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+c.Spawner.http_timeout = 60
