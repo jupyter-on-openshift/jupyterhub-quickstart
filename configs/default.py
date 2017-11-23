acfe29fbfcae6dc0d7e876fa8f69e3d29962720e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-11-23 19:00:35 +1100

    Further binding to KubeSpawner and stripping down what is installed.

--- configs/default.py
+++ configs/default.py
@@ -21,14 +21,15 @@ c.Spawner.port = 8080
 c.Spawner.hub_connect_ip = os.environ['JUPYTERHUB_SERVICE_HOST']
 c.Spawner.hub_connect_port = 8081
 
-c.Spawner.cmd = ['jupyterhub-singleuser',
-        '--hub-api-url=http://%s:%d/hub/api' % (c.Spawner.hub_connect_ip,
-        c.Spawner.hub_connect_port)]
+c.Spawner.cmd = ['jupyterhub-singleuser']
+c.Spawner.args = ['--hub-api-url=http://%s:%d/hub/api' % (
+        c.Spawner.hub_connect_ip, c.Spawner.hub_connect_port)]
 
 c.JupyterHub.admin_access = True
 
 if os.environ.get('JUPYTERHUB_COOKIE_SECRET'):
-    c.JupyterHub.cookie_secret = os.environ['JUPYTERHUB_COOKIE_SECRET'].encode('UTF-8')
+    c.JupyterHub.cookie_secret = os.environ[
+            'JUPYTERHUB_COOKIE_SECRET'].encode('UTF-8')
 else:
     c.JupyterHub.cookie_secret_file = '/opt/app-root/data/cookie_secret'
 
@@ -39,22 +40,16 @@ if os.environ.get('JUPYTERHUB_DATABASE_PASSWORD'):
 else:
     c.JupyterHub.db_url = '/opt/app-root/data/database.sqlite'
 
-
-if not os.environ.get('JUPYTERHUB_AUTHENTICATOR'):
-    c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
-
-elif os.environ.get('JUPYTERHUB_AUTHENTICATOR') == 'GitHub':
-    c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
-    c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
-    c.GitHubOAuthenticator.client_id = os.environ['OAUTH_CLIENT_ID']
-    c.GitHubOAuthenticator.client_secret = os.environ['OAUTH_CLIENT_SECRET']
+c.JupyterHub.authenticator_class = 'hashauthenticator.HashAuthenticator'
+c.HashAuthenticator.secret_key = os.environ.get(
+        'JUPYTERHUB_AUTHENTICATION_KEY', 'jupyterhub')
 
 whitelist = os.environ.get('JUPYTERHUB_USER_WHITELIST', '').strip()
-whitelist = whitelist and whitelist.split(',') or []
+whitelist = whitelist and whitelist.split() or []
 
 c.Authenticator.whitelist = set(whitelist)
 
 admin_users = os.environ.get('JUPYTERHUB_ADMIN_USERS', '').strip()
-admin_users = admin_users and admin_users.split(',') or []
+admin_users = admin_users and admin_users.split() or []
 
 c.Authenticator.admin_users = set(admin_users)
