c6201006e1fdebd9f58c7fc45aa51abe6767a0ee
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-09-13 12:23:36 +1000

    Allow setup using shell script before JupyterHub runs.

--- start-jupyterhub.sh
+++ start-jupyterhub.sh
@@ -6,6 +6,10 @@ set -eo pipefail
 
 PATH=$PATH:/opt/app-root/scripts
 
+# Read any custom environment variables.
+
+. /opt/app-root/src/jupyterhub_config.sh
+
 # Start the JupyterHub instance.
 
 exec jupyterhub -f /opt/app-root/etc/jupyterhub_config.py
